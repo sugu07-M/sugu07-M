af6dec0b9f680ebd594ee7077176c1264ac051a6
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-11 17:54:26 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,3 +5,15 @@ import { AppModule } from './app/app.module';
 import 'zone.js';
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
+#chart-container {
+    width: 50%; /* Set the width to 100% */
+    height: 350px; /* Set the height to 400 pixels */
+    box-sizing: border-box;
+  }
+  
+  /* Custom styles for the center label */
+  .ej2-accumulation-title {
+    font-size: 20px; /* Increase font size */
+    font-weight: bold; /* Make text bold */
+    color: #333; /* Set text color */
+  }
