90efe4008077bb2da727ee6b14825ed74b64304d
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 23:37:12 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,13 @@
 https://static.infragistics.com/xplatform/css/samples/
 */
 X [ERROR] NG8002: Can't bind to 'chartType' since it isn't a known property of 'canvas'. [plugin angular-compiler]
+PS D:\IT-Portal\IT-Portal\IT-Portal.UI> npm install apx-chart --save
+npm ERR! code E404
+npm ERR! 404 Not Found - GET https://registry.npmjs.org/apx-chart - Not found
+npm ERR! 404
+npm ERR! 404  'apx-chart@*' is not in this registry.
+npm ERR! 404
+npm ERR! 404 Note that you can also install from a
+npm ERR! 404 tarball, folder, http url, or git url.
 
-    src/app/dashboard/dashboard.component.html:254:78:
-      254 │ ...[labels]="doughnutChartLabels" [chartType]="'doughnut'"></canvas>
-          ╵                                   ~~~~~~~~~~~~~~~~~~~~~~~~
-
-  Error occurs in the template of component DashboardComponent.
-
-    src/app/dashboard/dashboard.component.ts:11:15:
-      11 │   templateUrl: './dashboard.component.html',
-         ╵                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+npm ERR! A complete log of this run can be found in: C:\Users\mts.user5\AppData\Local\npm-cache\_logs\2024-03-12T18_06_29_260Z-debug-0.log
