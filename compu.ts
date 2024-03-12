ba6fdd407c982f817ad8b223b7334ce6dcb80162
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 22:38:48 +0530

    Update compu.ts

--- compu.ts
+++ compu.ts
@@ -12,8 +12,15 @@ tooltip: {
   }
 },
 
-X [ERROR] TS2564: Property 'chart' has no initializer and is not definitely assigned in the constructor. [plugin angular-compiler]
+ [ERROR] NG2: Type 'ApexTitleSubtitle | undefined' is not assignable to type 'ApexTitleSubtitle'.
+  Type 'undefined' is not assignable to type 'ApexTitleSubtitle'. [plugin angular-compiler]
 
-    src/app/dashboard/dashboard.component.ts:34:22:
-      34 │   @ViewChild("chart") chart: ChartComponent;
-         ╵                       ~~~~~
+    src/app/dashboard/dashboard.component.html:277:20:
+      277 │                    [title]="chartOptions.title"></apx-chart>
+          ╵                     ~~~~~
+
+  Error occurs in the template of component DashboardComponent.
+
+    src/app/dashboard/dashboard.component.ts:27:15:
+      27 │   templateUrl: './dashboard.component.html',
+         ╵                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
