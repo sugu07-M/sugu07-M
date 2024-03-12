3385538e1480a2c096faf82d71d59f37fbe75c42
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 15:58:10 +0530

    Update compu.ts

--- compu.ts
+++ compu.ts
@@ -1,14 +1,24 @@
-import { ChartDataSets } from 'chart.js';
- public doughnutChartLabels: string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+ public doughnutChartLabels: string[] = ['Part 1', 'Part 2', 'Part 3'];
   public doughnutChartData: number[] = [350, 450, 100];
   public doughnutChartType: string = 'doughnut';
 
-  // events
-  public changeData(): void {
-    this.doughnutChartLabels = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'google Sales', 'TianMao Sales'];
-    this.doughnutChartData = [350, 450, 100, 80, 90];
+  public chartHovered(event: any): void {
+    if (event.active.length > 0) {
+      const chartData = event.active[0]._chart.data.datasets[0].data;
+      const hoveredPartIndex = event.active[0]._index;
+      const hoveredPartValue = chartData[hoveredPartIndex];
+      console.log(`Hovered Part: ${this.doughnutChartLabels[hoveredPartIndex]} - Value: ${hoveredPartValue}`);
+    }
   }
 
 
+<canvas baseChart
+        [data]="doughnutChartData"
+        [labels]="doughnutChartLabels"
+        [chartType]="doughnutChartType"
+        (chartHover)="chartHovered($event)">
+</canvas>
 
-<canvas baseChart [data]="doughnutChartData" [labels]="doughnutChartLabels" [chartType]="'doughnut'"></canvas>
+
+
+
