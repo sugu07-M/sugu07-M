4d3f083d5e2e606fe953d0cab85e74c5e6b0c370
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 11:02:21 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppModule } from './app/app.module';
 
-import 'zone.js';
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
-#chart-container {
-    width: 50%; /* Set the width to 100% */
-    height: 350px; /* Set the height to 400 pixels */
-    box-sizing: border-box;
-  }
-  
-  /* Custom styles for the center label */
-  .ej2-accumulation-title {
-    font-size: 20px; /* Increase font size */
-    font-weight: bold; /* Make text bold */
-    color: #333; /* Set text color */
-  }
+
+/* styles are loaded the Shared CSS file located at:
+https://static.infragistics.com/xplatform/css/samples/
+*/
