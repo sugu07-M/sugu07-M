881c1319c564007be3bad1f7051d741c5fa19e1a
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 15:28:10 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,15 @@
 /* styles are loaded the Shared CSS file located at:
 https://static.infragistics.com/xplatform/css/samples/
 */
-NG8002: Can't bind to 'data' since it isn't a known property of 'canvas'.
+X [ERROR] NG8002: Can't bind to 'chartType' since it isn't a known property of 'canvas'. [plugin angular-compiler]
+
+    src/app/dashboard/dashboard.component.html:254:78:
+      254 │ ...[labels]="doughnutChartLabels" [chartType]="'doughnut'"></canvas>
+          ╵                                   ~~~~~~~~~~~~~~~~~~~~~~~~
+
+  Error occurs in the template of component DashboardComponent.
+
+    src/app/dashboard/dashboard.component.ts:11:15:
+      11 │   templateUrl: './dashboard.component.html',
+         ╵                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
