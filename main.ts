8a8351ec6a2bb7d7e6c6de458c43676cc877ffba
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 14:46:45 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,3 +3,4 @@
 /* styles are loaded the Shared CSS file located at:
 https://static.infragistics.com/xplatform/css/samples/
 */
+NG8002: Can't bind to 'data' since it isn't a known property of 'canvas'.
