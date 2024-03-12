fda3379e8c68af906242ebae655507fce5582f08
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-12 22:29:57 +0530

    Update compu.ts

--- compu.ts
+++ compu.ts
@@ -11,3 +11,9 @@ tooltip: {
     }
   }
 },
+
+X [ERROR] TS2564: Property 'chart' has no initializer and is not definitely assigned in the constructor. [plugin angular-compiler]
+
+    src/app/dashboard/dashboard.component.ts:34:22:
+      34 │   @ViewChild("chart") chart: ChartComponent;
+         ╵                       ~~~~~
