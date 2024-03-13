b7e9a3b4248057673ab5ef5cb1cdfd9709f6f622
sugu <151340260+sugu07-M@users.noreply.github.com>
2024-03-13 12:53:54 +0530

    Update compu.ts

--- compu.ts
+++ compu.ts
@@ -1,70 +1,57 @@
-import {
-  ApexAxisChartSeries,
-  ApexChart,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
-  ApexLegend,
-  ApexXAxis,
-  ApexStroke,
-  ApexTooltip,
-  ApexFill,
-  ApexStates,
-  ApexTitleSubtitle
-} from "ng-apexcharts";
-
-export type ChartOptions = {
-  series?: ApexAxisChartSeries[] | ApexNonAxisChartSeries[];
-  chart?: ApexChart;
-  dataLabels?: ApexDataLabels;
-  plotOptions?: ApexPlotOptions;
-  yaxis?: ApexYAxis | ApexYAxis[];
-  legend?: ApexLegend;
-  xaxis?: ApexXAxis;
-  stroke?: ApexStroke;
-  tooltip?: ApexTooltip;
-  fill?: ApexFill;
-  states?: ApexStates;
-  title?: ApexTitleSubtitle;
-};
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css',
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+export class AppComponent {
+  public barChartOptions: ChartOptions = {
+    responsive: true
+  };
+  public barChartType: ChartType = 'horizontalBar';
+  public barChartLegend = true;
 
-export class DashboardComponent implements OnInit {
+  public barChartData: ChartDataSets[] = [
+    { data: [1, 2, 3], label: 'Approved', stack: 'a' },
+    { data: [1, 2, 3], label: 'Accepted', stack: 'a' },
+    { data: [1, 2, 3], label: 'Open', stack: 'a' },
+    { data: [1, 2, 3], label: 'In Progress', stack: 'a' },
+  ];
+  public barChartLabels: string[] = ['P', 'R', 'B'];
 
+  constructor() { }
 
-  @ViewChild("chart") chart: ChartComponent;
-  public chartOptions: Partial<ChartOptions>;
-  constructor(private http: HttpClient) {
-    this.chartOptions = {
-      series: [{
-        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-      }],
-      chart: {
-        type: 'line',
-        height: 350
-      },
-      dataLabels: {
-        enabled: false
-      },
-      plotOptions: {
-        bar: {
-          horizontal: false
-        }
-      },
-      xaxis: {
-        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
-      },
-      stroke: {
-        curve: 'smooth'
-      },
-      tooltip: {
-        enabled: true
-      }
-    }
+  ngOnInit() {
   }
-  
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    const data = [
+      Math.round(Math.random() * 100),
+      59,
+      80,
+      (Math.random() * 100),
+      56,
+      (Math.random() * 100),
+      40];
+    const clone = JSON.parse(JSON.stringify(this.barChartData));
+    clone[0].data = data;
+    this.barChartData = clone;
+    /**
+     * (My guess), for Angular to recognize the change in the dataset
+     * it has to change the dataset variable directly,
+     * so one way around it, is to clone the data, change it and then
+     * assign it;
+     */
+  }
+}
